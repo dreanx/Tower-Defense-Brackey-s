8985e6cdb152306bfa4c2f515692a9ebef1c4c3a
dreanx <pierre.basck@gmail.com>
2023-05-28 13:38:50 +0200

    Enemy AI

---
+++ Assets/Enemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.wayPoints[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+    }
+
+}
